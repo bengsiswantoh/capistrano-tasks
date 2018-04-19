6b88b7d8ae3f18f49a2616328ffcac3ec2ad5373
Bengsiswanto Hendrawan <bengsis@dwp.co.id>
2018-04-19 11:16:02 +0700

    feat(simplify): setting application link change to server hostname

--- lib/capistrano_tasks/mattermost.rb
+++ lib/capistrano_tasks/mattermost.rb
@@ -1,41 +1,32 @@
 require 'json'
 
+def send_notif(hostname, process)
+  # Send 1st notif
+  application_link = fetch(:application_link)
+  address = fetch(:mattermost)
+  data = {}
+  data[:username] = fetch(:mattermost_username)
+  data[:channel] = fetch(:mattermost_channel)
+  data[:text] = "@channel #{process} deploy #{hostname}"
+  execute %{curl -i -X POST -d 'payload=#{data.to_json}' #{address}}
+
+  # Send 2nd notif
+  data[:channel] = fetch(:mattermost_notify_me)
+  execute %{curl -i -X POST -d 'payload=#{data.to_json}' #{address}} if data[:channel].present?
+end
+
 namespace :mattermost do
   desc 'Info start mattermost'
   task :info_start do
-    on roles(:app) do
-      application_link = fetch(:application_link)
-      address = fetch(:mattermost)
-      data = {}
-      data[:username] = fetch(:mattermost_username)
-      data[:channel] = fetch(:mattermost_channel)
-      data[:text] = "@channel start deploy #{application_link} "
-      json = data.to_json
-      execute %{curl -i -X POST -d 'payload=#{json}' #{address}}
-      data[:channel] = fetch(:mattermost_notify_me)
-      if data[:channel] != ""
-        json = data.to_json
-        execute %{curl -i -X POST -d 'payload=#{json}' #{address}}
-      end
+    on roles(:app) do |server|
+      send_notif(server.hostname, "start")
     end
   end
 
   desc 'Info done mattermost'
   task :info_done do
-    on roles(:app) do
-      application_link = fetch(:application_link)
-      address = fetch(:mattermost)
-      data = {}
-      data[:username] = fetch(:mattermost_username)
-      data[:channel] = fetch(:mattermost_channel)
-      data[:text] = "@channel done deploy #{application_link}"
-      json = data.to_json
-      execute %{curl -i -X POST -d 'payload=#{json}' #{address}}
-      data[:channel] = fetch(:mattermost_notify_me)
-      if data[:channel] != ""
-        json = data.to_json
-        execute %{curl -i -X POST -d 'payload=#{json}' #{address}}
-      end
+    on roles(:app) do |server|
+      send_notif(server.hostname, "finish")
     end
   end
 end
