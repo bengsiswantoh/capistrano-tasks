6ed7032cd6525eec05cf8c64fbee812661c496e5
Bengsiswanto Hendrawan <bengsis@Bengsiswanto-PC.dwp.co.id>
2019-01-24 10:49:34 +0700

    refactor(spa): create function

--- lib/capistrano_tasks/spa.rb
+++ lib/capistrano_tasks/spa.rb
@@ -1,11 +1,16 @@
-build_directory = fetch(:spa_build_directory)
+def download_htaccess(path)
+  build_directory = fetch(:spa_build_directory)
+  file = "https://gist.github.com/mightiesthero/2c546203392237dd13f430edbf2e1d3a/raw/.htaccess"
+
+  execute "cd #{path}/#{build_directory} && wget #{file}"
+end
 
 namespace :spa do
   namespace :current do
     desc "Download htaccess within current path"
     task :download_htaccess do
       on roles(:app) do
-        execute "cd #{current_path}/#{build_directory} && wget https://gist.github.com/mightiesthero/2c546203392237dd13f430edbf2e1d3a/raw/.htaccess"
+        download_htaccess(current_path)
       end
     end
   end
@@ -14,7 +19,7 @@ namespace :spa do
     desc "Download htaccess within current path"
     task :download_htaccess do
       on roles(:app) do
-        execute "cd #{release_path}/#{build_directory} && wget https://gist.github.com/mightiesthero/2c546203392237dd13f430edbf2e1d3a/raw/.htaccess"
+        download_htaccess(release_path)
       end
     end
   end
